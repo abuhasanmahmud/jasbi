de1d230deb862f916a4b74c2645367345ad18df6
abuhasanmahmud <mahmud.me420@gmail.com>
2023-10-19 18:51:52 +0600

    featV(-1) final update

--- src/redux/api/apiSlice.js
+++ src/redux/api/apiSlice.js
@@ -1,7 +1,7 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-// const baseQuery = fetchBaseQuery({ baseUrl: "https://jasbi-back.vercel.app/" });
-const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:4000/" });
+const baseQuery = fetchBaseQuery({ baseUrl: "https://jasbi-back.vercel.app/" });
+// const baseQuery = fetchBaseQuery({ baseUrl: "http://localhost:4000/" });
 
 export const apiSlice = createApi({
   baseQuery,
